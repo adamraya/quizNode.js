a6505df09595c41b0926f25cbb67afe0aa9ecdb8
adamraya <adamraya@me.com>
2015-07-31 01:35:07 +0200

    Add question

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 // Routes definition of credits
 router.get('/author', function(req, res) {
